9137ba6cebdfb6ecd90c8bf275ff5f1a0a2ede90
Petr Stipek <pipaslot@seznam.cz>
2020-04-06 19:56:49 +0200

    separate classes

---
+++ Pipaslot.Logging.Benchmark/DemoAppBenchmark.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.IO;
+using BenchmarkDotNet.Attributes;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Pipaslot.Logging.Demo;
+using Pipaslot.Logging.Demo.Controllers;
+
+namespace Pipaslot.Logging.Benchmark
+{
+    public class DemoAppBenchmark
+    {
+        private ValuesController _controller;
+
+        #region Setup
+
+        [GlobalSetup]
+        public void GlobalSetup()
+        {
+            var services = BuildServiceProvider();
+
+            _controller = services.GetRequiredService<ValuesController>();
+        }
+
+        private IServiceProvider BuildServiceProvider()
+        {
+            var services = new ServiceCollection();
+            var configurationBuilder = new ConfigurationBuilder();
+            var configuration = configurationBuilder.Build();
+            var startup = new Startup(configuration);
+            startup.ConfigureServices(services);
+            services.AddLogging();
+            services.AddTransient<ValuesController>();
+            
+            var lb = new LoggingBuilder(services);
+            lb.AddRequestLogger(Directory.GetCurrentDirectory(),LogLevel.Trace);
+
+            return services.BuildServiceProvider();
+        }
+        
+        #endregion
+
+        // Original: 21,000ns
+        [Benchmark]
+        public void With_Logging()
+        {
+            _controller.PerformActionWithLogging();
+        }
+
+        [Benchmark]
+        public void Without_Logging()
+        {
+            _controller.PerformActionWithoutLogging();
+        }
+    }
+}
---
+++ Pipaslot.Logging.Benchmark/LoggingBuilder.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace Pipaslot.Logging.Benchmark
+{
+    public class LoggingBuilder : ILoggingBuilder
+    {
+        public LoggingBuilder(IServiceCollection services)
+        {
+            Services = services;
+        }
+
+        public IServiceCollection Services { get; }
+    }
+}
