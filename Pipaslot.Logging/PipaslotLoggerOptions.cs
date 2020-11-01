d06b044401f2ccb077d18f82e84f6646e60d9c22
Petr Stipek <pipaslot@seznam.cz>
2020-11-02 00:07:37 +0100

    default log output path

--- Pipaslot.Logging/PipaslotLoggerOptions.cs
+++ Pipaslot.Logging/PipaslotLoggerOptions.cs
@@ -6,7 +6,7 @@ namespace Pipaslot.Logging
 {
     public class PipaslotLoggerOptions
     {
-        public string OutputPath { get; set; } = "";
+        public string OutputPath { get; set; } = "./logs";
         public bool IncludeScopes { get; set; }
         public bool IncludeMethods { get; set; }
     }
